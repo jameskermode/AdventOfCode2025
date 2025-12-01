73bc93301923d1f283186cad39189bc2fcc1402a
James Kermode <james.kermode@gmail.com>
2025-12-01 17:40:55 +0000

    cleanup julia

--- julia/day1.jl
+++ julia/day1.jl
@@ -10,13 +10,8 @@ function part_1(input)
     for line in input
         dir, amount = line[1], line[2:end]
         amount = parse(Int, amount)
-        if dir == 'R'
-            pos += amount
-        elseif dir == 'L'
-            pos -= amount
-        else
-            error("bad direction $dir")
-        end
+        sgn = dir == 'R' ? 1 : -1
+        pos += sgn * amount
         pos = mod(pos, 100)
         if pos == 0
             nzeros += 1
@@ -54,13 +49,7 @@ function part_2(input)
     for line in input
         dir, amount = line[1], line[2:end]
         amount = parse(Int, amount)
-        if dir == 'R'
-            sgn = +1
-        elseif dir == 'L'
-            sgn = -1
-        else
-            error("bad direction $dir")
-        end
+        sgn = dir == 'R' ? 1 : -1
         for idx in 1:amount
             pos = mod(pos + sgn, 100)
             if pos == 0
